e14ab02581ca4629066f75adcd22884f45c2b310
Rohan <113842997+Rohan00kumar@users.noreply.github.com>
2025-08-29 18:27:59 +0530

    update integration

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import yaml
 from data import load_data
 from backtest import rolling_backtest
+from flask import Flask,redirect,url_for,render_template,request
+
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        # Handle POST Request here
+        return render_template('index.html')
+    return render_template('index.html')
+
 
 def main():
     with open("config.yaml") as f:
@@ -12,5 +22,6 @@ def main():
     df.to_csv(config['output']['results_path'], index=False)
     print("Saved results to", config['output']['results_path'])
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=5000,debug=True)
